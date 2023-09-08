0bc3726a9d875cd5373ed3e61e5378e53f6429ad
YennyRL <yrivadineira@gmail.com>
2023-09-07 23:39:06 -0300

    creacion de clase y test

--- sitioTurnos/models.py
+++ sitioTurnos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+#las clases tienen este formato para luego persistir correctamente 
+# #en una bbdd
+class Usuario(models.Model): 
+    dni = models.CharField(max_length=10, primary_key=True)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    numero_telefono = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.dni
--- sitioTurnos/tests.py
+++ sitioTurnos/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from .models import Usuario
 # Create your tests here.
+
+class UsuarioTestCase(TestCase):
+    #setUp sirve para crear la clase a la que le vamos a 
+    # realizar el testeo
+    def setUp(self):
+        Usuario.objects.create(
+            dni="12345678",
+            nombre="Aldo",
+            apellido="Segovia",
+            email="aldoo.segovia@gmail.com",
+            numero_telefono="1139416887"
+        )
+
+    def test_usuario_str(self):
+        usuario = Usuario.objects.get(dni="12345678")
+        self.assertEqual(usuario.__str__(), "12345678")
